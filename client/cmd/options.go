1b68b58d7e30c79e103d2ebc7e70d5648737e23b
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-08-09 19:18:13 +0300

    промежуточный коммит

--- client/cmd/options.go
+++ client/cmd/options.go
@@ -14,13 +14,13 @@ type Options struct {
 	PassPhrase                 string                                    `short:"k" long:"pass-phrase" default:"supersecretkey123" description:"pass phrase used to encrypt secret information" required:"true"`
 	Register                   command.RegisterCommand                   `command:"register" alias:"r" description:"Register user"`
 	AddSecretLoginPassword     command.AddSecretLoginPasswordCommand     `command:"add-secret-login-password" alias:"alp" description:"Add secret login password"`
-	AddSecretBankingCard       command.AddSecretBankingCardCommand       `command:"add-secret-banking-card" alias:"ab" description:"Add secret banking card"`
+	AddSecretBankingCard       command.AddSecretBankingCardCommand       `command:"add-secret-banking-card" alias:"abc" description:"Add secret banking card"`
 	AddSecretText              command.AddSecretTextCommand              `command:"add-secret-text" alias:"at" description:"Add secret text"`
 	AddSecretBinary            command.AddSecretBinaryCommand            `command:"add-secret-binary" alias:"ab" description:"Add secret binary from file"`
-	ReplaceSecretLoginPassword command.ReplaceSecretLoginPasswordCommand `command:"replace-secret-login-password" alias:"alp" description:"Replace secret with login password"`
-	ReplaceSecretBankingCard   command.ReplaceSecretBankingCardCommand   `command:"replace-secret-banking-card" alias:"ab" description:"Replace secret with banking card"`
-	ReplaceSecretText          command.ReplaceSecretTextCommand          `command:"replace-secret-text" alias:"at" description:"Replace secret with text"`
-	ReplaceSecretBinary        command.ReplaceSecretBinaryCommand        `command:"replace-secret-binary" alias:"ab" description:"Replace secret with binary from file"`
+	ReplaceSecretLoginPassword command.ReplaceSecretLoginPasswordCommand `command:"replace-secret-login-password" alias:"rlp" description:"Replace secret with login password"`
+	ReplaceSecretBankingCard   command.ReplaceSecretBankingCardCommand   `command:"replace-secret-banking-card" alias:"rbc" description:"Replace secret with banking card"`
+	ReplaceSecretText          command.ReplaceSecretTextCommand          `command:"replace-secret-text" alias:"rt" description:"Replace secret with text"`
+	ReplaceSecretBinary        command.ReplaceSecretBinaryCommand        `command:"replace-secret-binary" alias:"rb" description:"Replace secret with binary from file"`
 	RemoveSecret               command.RemoveSecretCommand               `command:"remove-secret" alias:"rm" description:"Remove secret"`
 	GetSecret                  command.GetSecretCommand                  `command:"get-secret" alias:"g" description:"Get secret"`
 	GetAllSecrets              command.GetAllSecretsCommand              `command:"get-all-secrets" alias:"ga" description:"Get all secrets"`
@@ -36,6 +36,8 @@ func mustParseOptions(opts *Options) *flags.Parser {
 			if errors.Is(flagsErr, flags.ErrHelp) {
 				os.Exit(0)
 			}
+		default:
+			os.Exit(1)
 		}
 		slog.Error("Failed to parse options: %s", slog.Any("error", err))
 		os.Exit(1)
